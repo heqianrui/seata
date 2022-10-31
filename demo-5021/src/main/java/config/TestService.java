1dcf326a3843b377c358da5f461ee416977d4800
a364176773 <364176773@qq.com>
2022-10-31 16:40:00 +0800

    update

--- demo-5021/src/main/java/config/TestService.java
+++ demo-5021/src/main/java/config/TestService.java
@@ -10,26 +10,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestService {
 
-    @Autowired
-    @Qualifier("jdbcTemplate")
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    @Qualifier("qsJdbcTemplate")
-    private JdbcTemplate qsJdbcTemplate;
-
     @GlobalTransactional(rollbackFor = Exception.class)
     public void test1() throws Exception {
-        String sql = "INSERT INTO test1 VALUES (?,?)";
-        Object[] objects = {1, 1};
-        String xid = RootContext.getXID();
-        System.out.println(xid);
-//        smartDAO.update("", sql, objects);
-        jdbcTemplate.update(sql,objects);
-//        qsSmartDAO.update("", sql, objects);
-        qsJdbcTemplate.update(sql, objects);
         String xxid = RootContext.getXID();
         System.out.println(xxid);
-//        restTemplate.getForObject("http://localhost:8080/Clearing/test2", String.class);
     }
 }
